5fed9e3fa8880cef64fa11047c8c660a4f61c485
Nenoh95 <vivian.nenoh03@gmail.com>
2024-04-07 09:27:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const prompt = require ("prompt-sync")();
+
+let correctAnswers = 0;
+
+console.log("Welcome to the computer hardware Quiz!");
+
+const answer1 = prompt("What is the brain of the computer? ");
+const correct_answer1 = "CPU";
+if (answer1.toUpperCase() === correct_answer1) {
+    console.log("You got it correct!");
+    correctAnswers += 1
+} 
+else {
+    console.log("You got it wrong...!");
+}
+
+const answer2 = prompt("What is better a 3090ti or a 4060? ");
+const correct_answer2 = "3090ti";
+
+if (answer2.toLowerCase() === correct_answer2) {
+    console.log("You got it correct!");
+    correctAnswers += 1
+} 
+else {
+    console.log("You got it wrong...!");
+}
+
+const answer3 = prompt("What is the recomended RAM in 2023? ");
+const correct_answer3 = "16GB";
+
+if (answer3.toUpperCase() === correct_answer3) {
+    console.log("You got it correct!");
+    correctAnswers += 1
+} 
+else {
+    console.log("You got it wrong...!");
+}
